589f2d5aae31d9d331f565b5962eb458456331fd
Йордан Стойчев <capitanblack747@gmail.com>
2024-05-02 23:56:51 +0300

    Dependency injection

---
+++ ATMProject/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using ATMProject.Factories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ATMProject.Commands
+{
+	public static class DependencyInjection
+	{
+		public static ServiceProvider ConfigureServices()
+		{
+			ServiceCollection services = new ServiceCollection();
+
+			services.AddSingleton<Bank>();
+			services.AddSingleton<IUserRepository, UserRepository>();
+			services.AddTransient<IUserManager, UserManager>();
+			services.AddTransient<IUserFactory, UserFactory>();
+			services.AddTransient<CommandManager>();
+
+
+			return services.BuildServiceProvider();
+		}
+	}
+}
---
+++ ATMProject/Properties/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ATMProject.Properties
+{
+	internal class DependencyInjection
+	{
+		ServiceCollection services = new ServiceCollection();
+	}
+}
